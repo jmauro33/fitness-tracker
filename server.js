723439df7ff307f382d85761e9f6ce926f959d07
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-09 14:19:27 -0500

    working on getting server to run all pages

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 
 app.use(require("./routes/api.js"));
 
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
