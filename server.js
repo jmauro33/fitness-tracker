03c24f1173f027693cf9572fbe61a2ab7c263e39
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-10 13:35:34 -0500

    working on everything

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 });
 
 app.use(require("./routes/api.js"));
-
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
