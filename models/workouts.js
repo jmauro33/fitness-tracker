487f277f4d9fee73531dd53d9f2ec6fc89591beb
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-11 09:47:02 -0500

    updated workout schema

--- models/workouts.js
+++ models/workouts.js
@@ -3,18 +3,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-      type: String,
+   type: {
+      type: string,
       trim: true,
       required: "exercise-type"
     },
-    value: {
-      type: Number,
-      required: "enter workout stats"
+    name: {
+      type: String,
+      trim: true,
+      required: "workout-type"
     },
-    date: {
-      type: Date,
-      default: Date.now
+    weight: {
+      type: number,
+      trim: true,
+      required: "weight-amount"
+    },
+    sets: {
+      type: number,
+      trim: true,
+      required: "number-of-sets"
+    },
+    reps:{
+      type: number,
+      trim: true,
+      required: "number-of-reps"
+    },
+    duration:{
+      type: number,
+      trim: true,
+      required: "duration-of-exercise"
     }
   });
   
